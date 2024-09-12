4398daf85c1a822b37b7af713021b59765ae0bf2
sivagurunath <sivagurunath@ap2002.chtc.wisc.edu>
2024-09-12 09:30:10 -0500

    Added the updated executable and submit file - mutliple jobs
    The new files allows to submit multiple jobs using a single submit file. The variables.txt has the different parameters mentioned which are read by the submit file and passed onto executable file

--- executable.sh
+++ executable.sh
@@ -1,6 +1,12 @@
 #echo "Hello World"
 mkdir -p model/model1
-python FromScratch.py --training_source sourcedata --training_target targetdata --model_path model --number_of_epochs 200 --testing_source 18_C1_all.tif --testing_target 18_C1_all_masks.tif --output_directory output --source_path 070721_Slide2_Animal1_all.tif
+echo "program args $0 , $1 , $2 , $3 , $4 ,  $@"
+echo "executing FromScratch.py..."
+#epoch=$1
+#model_name=$2
+#batch_size=$3
+#optimizer=$4
+python FromScratch.py --training_source sourcedata --training_target targetdata --model_path model --number_of_epochs $1 --model_name $2 --batch_size $3  --testing_source 18_C1_all.tif --testing_target 18_C1_all_masks.tif --output_directory output --source_path 070721_Slide2_Animal1_all.tif
 #python /test.py --training_source 17_C1_all.tiff --training_target 17_C1_all_masks.tif  --model_path model --number_of_epochs 1 --testing_source 17_C1_all.tiff --testing_target 17_C1_all_masks.tif --output_directory output --source_path 17_C1_all.tiff
 
 tar -czf output.tar.gz output
